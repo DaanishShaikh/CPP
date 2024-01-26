6e682c50cd85ba26778e6f023f0e97ce78ed33b1
DaanishShaikh <daanish222@gmail.com>
2024-01-26 16:58:00 +0530

    hi

--- beautiful_matrix.cpp
+++ beautiful_matrix.cpp
@@ -1,16 +1,26 @@
 #include<iostream>
 using namespace std;
 int main(){
-    bool n[100];
-    for(int j=0;j<=5;j++){
-        for(int i=0; i<=5; i++){
-            cin>>n[i];
-            cout<<n[i];
-            if(n[i]==1){
-                break;
-            }
-        }
-        
+    int x;
+    int y;
+    int n[5][5];
+    for(int j=0;j<=4;j++){
+        for(int i=0; i<=4; i++){
+            cin>>n[j][i];
+            if(n[i][j]==1){
+            x = i;
+            y = j;
+            }           
+        }   
     }
+    int z=2-x;
+    int u=2-y;
+    if (z<0){
+        z=z*(-1);
+    }
+    if(u<0){
+            u=u*(-1);
+    }    
+    cout<<z+u;
     return 0;
 }
