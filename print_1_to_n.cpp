6d9ee03316c1203ce90c52f76d01db1317817312
DaanishShaikh <daanish222@gmail.com>
2024-02-06 22:06:43 +0530

    update

---
+++ print_1_to_n.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int z;
+    cin>>z;
+    for(int i=1 ; i<=z ; i++){
+        cout<<i;
+    }
+    
+    return 0;
+}
--- vector.cpp
+++ vector.cpp
@@ -6,6 +6,12 @@ int main(){
     int x=v.size();
     cout<<x;
     v={1,2,3};
+    for(int i=0 ; i<3 ; i++){
+        cin>>v[i];
+    }
+    for(int k=0 ; k<3 ; k++){
+        cout<<v[k]<<endl;
+    }
     cout<<x;
     return 0;
 }
