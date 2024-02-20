a4ccd80f1342d8c184d43723b5aa253b6efe1794
DaanishShaikh <daanish222@gmail.com>
2024-02-20 10:27:46 +0530

    20-02-2024

---
+++ binary_search1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lowerboud(int a; int x);
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0 ; i<n ; i++){
+        cin>>a[i];
+    }
+    a.sort(begin,end);
+    int lowerboud(int a,int x){
+        int low=0, high=a.size()-1, ans=a.size();
+        while(low<high){
+           int mid=(low+high)/2;
+        }
+    }
+    
+}
---
+++ binary_search_bitonic.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[6]={1,4,10,8,6,2};
+    int max;
+    int mx(int a,int max){
+        int low=0, high=a.size()-1 , max=a.size()-1;
+
+    }
+}
