c7d7e562f67544ca0c5550945546f3283f24a996
DaanishShaikh <daanish222@gmail.com>
2024-03-10 19:52:56 +0530

    10-03-2024

--- binary_search_assignment.cpp
+++ binary_search_assignment.cpp
@@ -69,10 +69,10 @@ using namespace std;
 //  }
  
  
- int main(){
-    int n;
-    cin>>n;
-    vector<int>v(n);
+//  int main(){
+//     int n;
+//     cin>>n;
+//     vector<int>v(n);
     for(int i=0 ; i<n ; i++){
         cin>>v[i];
     }
