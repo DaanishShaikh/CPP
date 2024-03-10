99056bc0dac6ff47cb1a6a58214832d4fdfc63bf
DaanishShaikh <daanish222@gmail.com>
2024-03-10 19:50:58 +0530

    10-03-2024

--- binary_search_assignment.cpp
+++ binary_search_assignment.cpp
@@ -1,15 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
- int lowerbound(vector<int>v,int x){
-    int upper=v.size()-1, lower=0, mid=(upper+lower)/2;
-    while(upper>=lower){
-        if(upper==lower+1){
-            return v[upper];
-        }
-        else if(x==v[mid]){
-            return v[mid+1];
-            break;
-        }
+//  int lowerbound(vector<int>v,int x){
+//     int upper=v.size()-1, lower=0, mid=(upper+lower)/2;
+//     while(upper>=lower){
+//         if(upper==lower+1){
+//             return v[upper];
+//         }
+//         else if(x==v[mid]){
+//             return v[mid+1];
+//             break;
+//         }
         else if(x>v[mid]){
             lower=mid;
             mid=(upper+lower)/2;
