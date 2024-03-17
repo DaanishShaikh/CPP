013e1b3dead703a64b3bea4b6ec8ff60dc416370
DaanishShaikh <daanish222@gmail.com>
2024-03-17 19:53:11 +0530

    17-03-2024

--- pattern_printing_v.cpp
+++ pattern_printing_v.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    string s={'H','e','l','l','o',' ','w','o','r','l','d'};
+    string s={'H','e','l','l','o',' ','W','o','r','l','d'};
     cout<<s;
+
 }
