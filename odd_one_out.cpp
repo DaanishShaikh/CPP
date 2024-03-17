539fefe61ebe361e29b935fcdacfb15f27c7f8ed
DaanishShaikh <daanish222@gmail.com>
2024-03-17 19:37:23 +0530

    17-03-2024

--- odd_one_out.cpp
+++ odd_one_out.cpp
@@ -1,23 +1,3 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int tt;
-    cin>>tt;
-    while(tt--){
-    int a, b, c;
-    cin >> a >> b >> c;
-    if (a == b) {
-        cout << c << "\n";
-    }
-    else if (a == c) {
-        cout << b << "\n";
-    }
-    else {
-        cout << a << "\n";
-    }
-    cout<<pow(a,b); 
-    }
-       
-    return 0;
-}
