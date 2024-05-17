a6c30d19487f0064c77a7d52da7267c3e816ee95
DaanishShaikh <daanish222@gmail.com>
2024-05-17 17:18:39 +0530

    15-05-2024

---
+++ pattern_printing_striver_6.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    char c='A';
+    for(int i=0 ; i<n ; i++){
+        for(int j=0 ; j<i+1 ; j++){
+            cout<<c<<' ';
+        }
+        c++;
+        cout<<endl;
+    }
+}
